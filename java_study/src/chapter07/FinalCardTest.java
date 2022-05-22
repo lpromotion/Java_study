bdf7e8d0042e6fa894cd357b2d2f7071dd25b123
lpromotion <lpromotion00@gmail.com>
2022-05-22 23:32:47 +0900

    ex_7-FinalCardTest

---
+++ java_study/src/chapter07/FinalCardTest.java
@@ -0,0 +1,30 @@
+package chapter07;
+
+class Cardd {
+	final int NUMBER; // 상수지만 선언과 함께 초기화 하지 않고
+	final String KIND; // 생성자에서 단 한번만 초기화할 수 있다.
+	static int width  = 100;	
+	static int height = 250;
+
+	Cardd(String kind, int num) {	
+		KIND = kind;
+		NUMBER = num;
+	}
+
+	Cardd() {
+		this("HEART", 1);
+	}
+
+	public String toString() {
+		return KIND +" "+ NUMBER;
+	}
+}
+
+class FinalCardTest {
+	public static void main(String[] args) {
+		Cardd c = new Cardd("HEART", 10);
+		System.out.println(c.KIND);
+		System.out.println(c.NUMBER);
+		System.out.println(c);
+	}
+}
