c0ee242a2be75f50bcaece5c85c2e2ec0797b955
lpromotion <lpromotion00@gmail.com>
2022-06-14 23:50:33 +0900

    ex_7-BidingTest

---
+++ java_study/src/chapter07/BindingTest.java
@@ -0,0 +1,30 @@
+package chapter07;
+
+class BindingTest{
+	public static void main(String[] args) {
+		Parentt p = new Childd();
+		Childd c = new Childd();
+
+		System.out.println("p.x = " + p.x);
+		p.method();
+
+		System.out.println("c.x = " + c.x);
+		c.method();
+	}
+}
+
+class Parentt {
+	int x = 100;
+
+	void method() {
+		System.out.println("Parent Method");
+	}
+}
+
+class Childd extends Parentt {
+	int x = 200;
+
+	void method() {
+		System.out.println("Child Method");
+	}
+}
